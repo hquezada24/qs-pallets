a33926a81a50c84d1f7298c17d2c3ce1da4f178f
Hugo <hquezada@leomail.tamuc.edu>
2025-09-04 14:47:43 -0500

    feat: add line break to footer address span.

--- src/components/common/Footer/Index.jsx
+++ src/components/common/Footer/Index.jsx
@@ -26,7 +26,10 @@ const Footer = () => {
               >
                 <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
               </svg>
-              <span>1415 FM 2216, Honey Grove, TX 75446</span>
+              <span>
+                1415 FM 2216, <br />
+                Honey Grove, TX 75446
+              </span>
             </div>
             <div className={styles.contactItem}>
               <svg
