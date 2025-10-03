fb32c8b0e397023aa77a92719c679bbb98318599
Hugo <hquezada@leomail.tamuc.edu>
2025-10-03 12:36:06 -0500

    feat: add accessibility attributes

--- src/pages/Home/Components/Hero/Hero.jsx
+++ src/pages/Home/Components/Hero/Hero.jsx
@@ -3,10 +3,12 @@ import { Button } from "../../../../components/common/Button/Index";
 
 const Hero = () => {
   return (
-    <section className={styles.hero} role="banner">
+    <section className={styles.hero} id="hero" aria-labelledby="hero-title">
       <div className={styles.background}>
         <div className={styles.heroText}>
-          <h1 className={styles.title}>The Right Pallets, Every Time</h1>
+          <h1 className={styles.title} id="hero-title">
+            The Right Pallets, Every Time
+          </h1>
           <p className={styles.subtitle}>
             We provide reliable pallet supply and custom solutions so you can
             focus on your business, not your logistics
