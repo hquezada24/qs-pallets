32c79f8252af088437bfc8759b87e48c7b65c219
Hugo <hquezada@leomail.tamuc.edu>
2026-01-13 11:25:37 -0600

    add header navigation styling for active links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,22 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import { useState, useRef } from "react";
+import { usePathname } from "next/navigation";
 import "@/styles/Header.css";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+  const location = usePathname();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const isActiveLink = (path) => {
+    return location === path;
+  };
   return (
     <header className={`header `}>
       <div className="container">
@@ -25,22 +38,43 @@ const Header = () => {
         {/* Desktop Navigation */}
         <nav className="navigation" aria-label="Main navigation">
           <ul className="navList" role="list">
-            <li className="navItem">
+            <li className={`navItem ${isActiveLink("/") ? "activeLink" : ""}`}>
+              <Link href="/" className={`headerNavLink`}>
+                Home
+              </Link>
+            </li>
+            <li
+              className={`navItem ${
+                isActiveLink("/products") ? "activeLink" : ""
+              }`}
+            >
               <Link href="/products" className={`headerNavLink`}>
                 Products
               </Link>
             </li>
-            <li className="navItem">
+            <li
+              className={`navItem ${
+                isActiveLink("/request-a-quote") ? "activeLink" : ""
+              }`}
+            >
               <Link href="/request-a-quote" className={`headerNavLink`}>
                 Request a quote
               </Link>
             </li>
-            <li className="navItem">
+            <li
+              className={`navItem ${
+                isActiveLink("/about") ? "activeLink" : ""
+              }`}
+            >
               <Link href="/about" className={`headerNavLink`}>
                 About
               </Link>
             </li>
-            <li className="navItem">
+            <li
+              className={`navItem ${
+                isActiveLink("/contact") ? "activeLink" : ""
+              }`}
+            >
               <Link href="/contact" className={`headerNavLink`}>
                 Contact
               </Link>
@@ -49,9 +83,9 @@ const Header = () => {
         </nav>
 
         {/* Mobile Menu Button */}
-        {/* <div className="mobileMenu" ref={menuRef}>
+        <div className="mobileMenu" ref={menuRef}>
           <button
-            className="menu-button"
+            className="hamburger-button"
             onClick={toggleMenu}
             aria-expanded={isMenuOpen}
             aria-controls="mobile-navigation"
@@ -73,14 +107,74 @@ const Header = () => {
             aria-hidden={!isMenuOpen}
           >
             <ul className="mobileNavList" role="list">
-              <li className="mobileNavItem">
-                <Link href="/products" className={`mobileNavLink`}>
+              <li
+                className={`mobileNavItem ${
+                  isActiveLink("/") ? "activeMobileLink" : ""
+                }`}
+              >
+                <Link
+                  href="/"
+                  onClick={() => setIsMenuOpen(false)}
+                  className={`mobileNavLink`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li
+                className={`mobileNavItem ${
+                  isActiveLink("/products") ? "activeMobileLink" : ""
+                }`}
+              >
+                <Link
+                  href="/products"
+                  onClick={() => setIsMenuOpen(false)}
+                  className={`mobileNavLink`}
+                >
                   Products
+                </Link>
+              </li>
+              <li
+                className={`mobileNavItem ${
+                  isActiveLink("/request-a-quote") ? "activeMobileLink" : ""
+                }`}
+              >
+                <Link
+                  href="/request-a-quote"
+                  onClick={() => setIsMenuOpen(false)}
+                  className={`mobileNavLink`}
+                >
+                  Request a Quote
+                </Link>
+              </li>
+              <li
+                className={`mobileNavItem ${
+                  isActiveLink("/about") ? "activeMobileLink" : ""
+                }`}
+              >
+                <Link
+                  href="/about"
+                  onClick={() => setIsMenuOpen(false)}
+                  className={`mobileNavLink`}
+                >
+                  About
+                </Link>
+              </li>
+              <li
+                className={`mobileNavItem ${
+                  isActiveLink("/contact") ? "activeMobileLink" : ""
+                }`}
+              >
+                <Link
+                  href="/contact"
+                  onClick={() => setIsMenuOpen(false)}
+                  className={`mobileNavLink`}
+                >
+                  Contact
                 </Link>
               </li>
             </ul>
           </nav>
-        </div> */}
+        </div>
       </div>
     </header>
   );
