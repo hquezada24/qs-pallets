b4c2bb1e2cb04d3d7feab03caf1885c3944ce618
Hugo <hquezada@leomail.tamuc.edu>
2026-02-25 13:09:26 -0600

    Create dashboard main content

--- src/app/(dashboard)/dashboard/page.js
+++ src/app/(dashboard)/dashboard/page.js
@@ -1,7 +1,53 @@
-import DashboardHeader from "@/components/DashboardHeader";
+import DashboardNav from "@/components/DashboardNav";
+import DashboardHomeGraph from "@/components/DashboardHomeGraph";
 
 const DashboardHome = () => {
-  return <></>;
+  return (
+    <>
+      <style>{`
+    .dashboard-grid {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1.5rem;
+  padding: 1rem;
+  margin-top: 1rem;
+}
+
+.card {
+  background: white;
+  border-radius: 12px;
+  padding: 1.25rem;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
+}
+
+/* Desktop layout */
+@media (min-width: 1024px) {
+  .dashboard-grid {
+    grid-template-columns: 2fr 1fr;
+  }
+
+  .chart {
+    grid-column: -1 / span 2;
+  }
+
+  /* .table {
+    grid-column: 2 / 3;
+  } */
+}
+    `}</style>
+      <div className="items-center py-8">
+        <div className="nav-options flex justify-evenly">
+          <DashboardNav bg={"blue"} text={"New Orders"} />
+          <DashboardNav bg={"red"} text={"Open Quotes"} />
+          <DashboardNav bg={"green"} text={"Total Inventory"} />
+          <DashboardNav bg={"yellow"} text={"Customers"} />
+        </div>
+        <div className="graphs-tables dashboard-grid bg-gray-200 ">
+          <DashboardHomeGraph className="card chart" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DashboardHome;
