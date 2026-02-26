de08b62223be681a2cfc8fa41f32c2d0943157b2
Hugo <hquezada@leomail.tamuc.edu>
2026-02-26 13:18:33 -0600

    Modify proxy to redirect host/dashboard to dashboard.host

--- src/proxy.js
+++ src/proxy.js
@@ -2,17 +2,28 @@ import { NextResponse } from "next/server";
 
 export function proxy(req) {
   const host = req.headers.get("host") || "";
+  const { pathname, protocol } = req.nextUrl;
+
+  const isLocalhost = host.includes("localhost");
+  const baseDomain = isLocalhost
+    ? "localhost:3000"
+    : host.split(".").slice(-2).join(".");
+
+  const dashboardSubdomain = `dashboard.${baseDomain}`;
 
   console.log("Host: ", host);
 
-  if (host.startsWith("dashboard.")) {
+  if (host === baseDomain && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(`${protocol}//${dashboardSubdomain}`);
+  }
+
+  if (host === dashboardSubdomain) {
     return NextResponse.rewrite(new URL("/dashboard", req.url));
   }
 
   return NextResponse.next();
 }
 
-// MUY IMPORTANTE
 export const config = {
   matcher: "/((?!_next|favicon.ico).*)",
 };
