2689595bb022e5f3f431ff5c22035739251c0149
Hugo <hquezada@leomail.tamuc.edu>
2026-03-06 13:14:56 -0600

    Add loading, error, and submitting status states to users page

--- src/app/(dashboard)/users/page.js
+++ src/app/(dashboard)/users/page.js
@@ -8,11 +8,15 @@ const Users = () => {
   const [form, setForm] = useState({
     name: "",
     email: "",
-    role: "",
+    role: "employee",
     password: "",
   });
 
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submitStatus, setSubmitStatus] = useState(null);
 
   const handleChange = (e) => {
     setForm({
@@ -23,36 +27,63 @@ const Users = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsSubmitting(true);
+    setSubmitStatus(null);
     try {
       // await apiRequest("/api/users", {
       //   method: "POST",
       //   body: JSON.stringify(form),
       // });
 
-      await apiRequest("/api/users", {
+      const res = await apiRequest("/api/users", {
         method: "POST",
         body: JSON.stringify(form),
       });
+
+      console.log("response: ", res.ok);
+      if (!res.ok) {
+        throw new Error("Failed to create user");
+      }
+
+      console.log("submitted successfully");
+      setForm({
+        name: "",
+        email: "",
+        role: "employee",
+        password: "",
+      });
     } catch (error) {
+      setSubmitStatus("error");
       console.error("User registration error: ", error.message);
+    } finally {
+      setIsSubmitting(false);
+      await fetchUsers();
     }
   };
 
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const res = await apiRequest("/api/users");
+  async function fetchUsers() {
+    try {
+      setLoading(true);
+      setSubmitStatus(null);
+      setError(null);
 
-        if (!res.ok) {
-          throw new Error("Failed to fetch data");
-        }
+      const res = await apiRequest("/api/users");
 
-        const data = await res.json();
-        setUsers(data);
-      } catch (error) {
-        console.error("User fetching error: ", error.message);
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
       }
+
+      const data = await res.json();
+      setUsers(data);
+    } catch (error) {
+      setError(error.message);
+      console.error("User fetching error: ", error.message);
+    } finally {
+      setLoading(false);
     }
+  }
+
+  useEffect(() => {
     fetchUsers();
   }, []);
 
@@ -63,17 +94,27 @@ const Users = () => {
   ];
 
   return (
-    <div className="p-8 flex justify-evenly">
+    <div className="p-8 flex flex-col sm:flex-row justify-evenly space-y-4">
       <div className="users-left">
-        {users && <Table columns={userColumns} data={users.users} />}
+        {loading && <p>Loading users...</p>}
+
+        {error && <p className="text-red-500">{error}</p>}
+
+        {!loading && !error && (
+          <Table title={"Users"} columns={userColumns} data={users.users} />
+        )}
       </div>
       <div className="users-">
         <h2 className="mb-3">Add a New User</h2>
+        {submitStatus === "error" && (
+          <h3 className="text-red-500 ">Could Not Create New User!</h3>
+        )}
         <Form
           form={form}
           handleSubmit={handleSubmit}
           handleChange={handleChange}
           newUser={true}
+          submitType={isSubmitting ? "Creating user..." : "Create User"}
         />
       </div>
     </div>
